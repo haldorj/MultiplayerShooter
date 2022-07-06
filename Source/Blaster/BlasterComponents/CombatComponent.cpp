5f953e0dfdbc3e58d6ab8e69e16a06c50fc56064
haldorj <haldorjh@gmail.com>
2022-07-06 20:26:03 +0200

    Spawning Projectile

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -112,9 +112,11 @@ void UCombatComponent::TraceUnderCrosshairs(FHitResult& TraceHitResult)
 		if (!TraceHitResult.bBlockingHit)
 		{
 			TraceHitResult.ImpactPoint = End;
+			HitTarget = End;
 		}
 		else
 		{
+			HitTarget = TraceHitResult.ImpactPoint;
 			DrawDebugSphere(
 				GetWorld(),
 				TraceHitResult.ImpactPoint,
@@ -138,7 +140,7 @@ void UCombatComponent::MulticastFire_Implementation()
 	if (Character)
 	{
 		Character->PlayFireMontage(bAiming);
-		EquippedWeapon->Fire();
+		EquippedWeapon->Fire(HitTarget);
 	}
 }
 
--- Source/Blaster/Weapon/ProjectileWeapon.h
+++ Source/Blaster/Weapon/ProjectileWeapon.h
@@ -13,6 +13,8 @@ UCLASS()
 class BLASTER_API AProjectileWeapon : public AWeapon
 {
 	GENERATED_BODY()
+public:
+	virtual void Fire(const FVector& HitTarget) override;
 
 private:
 	UPROPERTY(EditAnywhere)
