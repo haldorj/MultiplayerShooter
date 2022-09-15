b04c351707efccc7b5d7011648cf90998ec1e133
haldorj <haldorjh@gmail.com>
2022-09-15 23:58:24 +0200

    Submachine Gun

--- Source/Blaster/BlasterComponents/CombatComponent.h
+++ Source/Blaster/BlasterComponents/CombatComponent.h
@@ -140,6 +140,9 @@ private:
 	UPROPERTY(EditAnywhere)
 	int32 StartingPistolAmmo = 0;
 
+	UPROPERTY(EditAnywhere)
+	int32 StartingSMGAmmo = 0;
+
 	void InitializeCarriedAmmo();
 
 	UPROPERTY(ReplicatedUsing = OnRep_CombatState)
