f26da79c442825a587c2d3c48edf226932a5c7ca
haldorj <haldorjh@gmail.com>
2022-09-18 15:30:48 +0200

    Grenade Assets

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -189,6 +189,13 @@ private:
 
 	UPROPERTY()
 	class ABlasterPlayerState* BlasterPlayerState;
+
+	//
+	// Grenade
+	//
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* AttachedGrenade;
 public:	
 	void SetOverlappingWeapon(AWeapon* Weapon);
 	bool IsWeaponEquipped();
