8aa697c33f20c5b0e3a034f423b915a7b6af0c2b
haldorj <haldorjh@gmail.com>
2022-10-01 06:47:41 +0200

    Show Widget Locally

--- Source/Blaster/Weapon/HitScanWeapon.cpp
+++ Source/Blaster/Weapon/HitScanWeapon.cpp
@@ -98,6 +98,9 @@ void AHitScanWeapon::WeaponTraceHit(const FVector& TraceStart, const FVector& Hi
 		{
 			BeamEnd = OutHit.ImpactPoint;
 		}
+
+		DrawDebugSphere(GetWorld(), BeamEnd, 16.f, 12, FColor::Orange, true);
+
 		if (BeamParticles)
 		{
 			UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(
